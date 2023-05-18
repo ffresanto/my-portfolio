a4f82901ebeed44b3e9bfc4c103630bc79fc4c31
ffresanto <ffresanto@gmail.com>
2023-05-18 15:13:44 -0300

    animacao nas section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,34 @@
 if (window.SimpleAnime) {
     new SimpleAnime();
 }
+
+var root = document.documentElement;
+root.className += ' js';
+
+function boxTop(idBox) {
+	var boxOffset = $(idBox).offset().top;
+	return boxOffset;
+}
+
+$(document).ready(function() {
+	var $target = $('.anime-scroll'),
+			animationClass = 'anime-init',
+			windowHeight = $(window).height(),
+			offset = windowHeight - (windowHeight / 4);
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+		$target.each(function() {
+			if (documentTop > boxTop(this) - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+	animeScroll();
+
+	$(document).scroll(function() {
+		setTimeout(function() {animeScroll()}, 150);
+	});
+});
